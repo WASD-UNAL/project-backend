abf079125cab184c0b6e4ea2c9bd88fbac835e6b
sebasO1 <solayas@unal.edu.co>
2026-06-02 11:41:25 -0500

    fix(database): correct model types and database relations

--- src/main/kotlin/app/gymly/model/Plan.kt
+++ src/main/kotlin/app/gymly/model/Plan.kt
@@ -1,6 +1,7 @@
 package app.gymly.model
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity
 @Table(name = "plan")
@@ -18,8 +19,8 @@ class Plan(
     @Column(name = "duration_days", nullable = false)
     var durationDays: Int,
 
-    @Column(nullable = false)
-    var price: Int,
+    @Column(nullable = false, precision = 10, scale = 2)
+    var price: BigDecimal,
 
     @Column(nullable = false)
     var active: Boolean = true
